b919277b3c4898feeb809adcb4037e3d21e3a12b
heiro <dev.heiro@gmail.com>
2025-04-23 10:38:07 +0900

    chore : 수업

--- src/OnePieceMember.js
+++ src/OnePieceMember.js
@@ -3,8 +3,10 @@ export default function OnePieceMember({ emoji, role, desc, name }) {
         <div className="crew-card">
             <div className="img-placeholder">{emoji}</div>
             <div className="info">
+
                 <h2>{name}</h2>
                 <p><strong>{role}</strong> – {desc}</p>
+
             </div>
         </div>
     );
